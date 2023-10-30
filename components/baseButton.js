70f80720674e08dafccbc48b8cd27e7b07b9c2a7
DanielHonrales <danielhonrales@gmail.com>
2023-10-30 11:25:21 -0500

    First component

---
+++ components/baseButton.js
@@ -0,0 +1,9 @@
+import { Button } from "react-native"
+
+const BaseButton = () => {
+    return <Button
+    title="bruh"
+    onPress={() => Alert.Alert('why doe')}/>
+}
+
+export default BaseButton
