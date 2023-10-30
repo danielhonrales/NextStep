56ba08f1fde203e204942baffdc1c27f8729b4ab
DanielHonrales <danielhonrales@gmail.com>
2023-10-30 13:50:00 -0500

    Added global colors

--- components/baseButton.js
+++ components/baseButton.js
@@ -1,9 +1,7 @@
 import { Button } from "react-native"
 
-const BaseButton = () => {
+export const BaseButton = () => {
     return <Button
     title="bruh"
     onPress={() => Alert.Alert('why doe')}/>
 }
-
-export default BaseButton
---
+++ components/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    background: 'FFFFFF',
+    foreground: 'D9D9D9',
+    
+    accent: '469D89',
+    highlight: '183642',
+    negative: 'FF4444'
+}
