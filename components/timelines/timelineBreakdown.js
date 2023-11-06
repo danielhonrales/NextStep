ab25eea70ab446f24e2109f5cdd8d0f46ab2ed02
hanasafia <hanasafiaahmad@gmail.com>
2023-11-06 15:39:12 -0600

    importing from react native

--- components/timelines/timelineBreakdown.js
+++ components/timelines/timelineBreakdown.js
@@ -1,16 +1,18 @@
-import { TouchableOpacity, StyleSheet } from "react-native"
 import { FONTS } from "../fonts"
 import { COLORS } from "../colors"
 import { STEP_STATUS } from "./StepStatus"
 import {Image} from "../dropdownicon.png"
 import {Image} from "inprogress.png"
 import React from "react"
+import {View, Text, Image, ScrollView, TextInput, 
+    TouchableOpacity, Button, Stylesheet} from 'react-native';
+
 
 // when a specific timeline is clicked, open the page with the breakdown
 
-//export function openTimeline {} - 
-
-
+export function TimelineBreakdown ({Navigation} ) {
+    
+}
 // To close it, click it again 
 
 // to mark a task as complete, tap it and turn it green 
