6ee4f53115665c6c7a9e682494ab30c346925ded
DanielHonrales <danielhonrales@gmail.com>
2023-11-05 20:53:28 -0600

    Base timeline step component

---
+++ components/timelines/StepStatus.js
@@ -0,0 +1,4 @@
+export const STEP_STATUS ={
+    in_progress: 0,
+    done: 1
+}
---
+++ components/timelines/TimelimeStep.js
@@ -0,0 +1,82 @@
+import { TouchableOpacity, StyleSheet } from "react-native"
+import { FONTS } from "../fonts"
+import { COLORS } from "../colors"
+import { STEP_STATUS } from "./StepStatus"
+import React from "react"
+
+export function TimelineStep({name}) {
+    const [status, setStatus] = React.useState(STEP_STATUS.in_progress)
+    const [expanded, setExpanded] = React.useState(false);
+    const [substeps, setSubsteps] = React.useState([])
+
+    return <TouchableOpacity
+        style={styles.touchable}
+        onPress={() => console.log("Open step breakdown")}
+        >
+        <View style={styles.stepView}>
+            <Text style={styles.stepName}>{name}</Text>
+            <View style={styles.iconsView}>
+                <TouchableOpacity
+                style={styles.icon}
+                onPress={() => console.log("Change status")}
+                >
+                    <Image 
+                    source={require('./../../assets/inprogress.png')}
+                    style={styles.statusIcon}
+                    />
+                </TouchableOpacity>
+                
+                <TouchableOpacity
+                style={styles.icon}
+                onPress={() => console.log("expand")}
+                >
+                    <Image 
+                    source={require('./../../assets/dropdownIcon.png')}
+                    style={styles.dropdownIcon}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    </TouchableOpacity>
+}
+
+const styles = StyleSheet.create({
+    touchable: {
+        borderWidth: 3,
+        borderColor: COLORS.black,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '90%',
+        borderRadius: 35,
+    },
+    stepView: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    stepName: {
+        flex: 4,
+        fontSize: FONTS.subtitleSize,
+        alignContent: 'center',
+        justifyContent: 'flex-start',
+        paddingLeft: 5
+    },
+    iconsView: {
+        flex: 1,
+        flexDirection: 'column',
+        alignContent: 'center',
+        justifyContent: 'flex-end',
+        paddingRight: 5
+    },
+    icon: {
+        height: 15,
+        width: 45,
+    },
+    dropdownIcon: {
+        height: 15,
+        width: 45,
+    },
+    statusIcon: {
+        height: 15,
+        width: 45,
+    }
+})
