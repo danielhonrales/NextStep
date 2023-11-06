5e37f605171672d9b9c29e89884f17053f82e7b4
hanasafia <hanasafiaahmad@gmail.com>
2023-11-05 22:21:46 -0600

    import drop down image

--- components/timelines/timelineBreakdown.js
+++ components/timelines/timelineBreakdown.js
@@ -2,6 +2,7 @@ import { TouchableOpacity, StyleSheet } from "react-native"
 import { FONTS } from "../fonts"
 import { COLORS } from "../colors"
 import { STEP_STATUS } from "./StepStatus"
+import {Image} from "../dropdownicon.png"
 import React from "react"
 
 // when a specific timeline is clicked, open the page with the breakdown
