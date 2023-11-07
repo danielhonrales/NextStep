50f2aaf2ce114c3e806792e6a4646d334f606faf
hanasafia <hanasafiaahmad@gmail.com>
2023-11-07 15:13:46 -0600

    functions for TimelineBreakdown

--- components/timelines/timelineBreakdown.js
+++ components/timelines/timelineBreakdown.js
@@ -2,25 +2,26 @@ import { FONTS } from "../fonts"
 import { COLORS } from "../colors"
 import { STEP_STATUS } from "./StepStatus"
 import {Image} from "../dropdownicon.png"
-import {Image} from "inprogress.png"
+import {Image} from "../inprogress.png"
 import React from "react"
 import {View, Text, Image, ScrollView, TextInput, 
     TouchableOpacity, Button, Stylesheet} from 'react-native';
 
 
-// when a specific timeline is clicked, open the page with the breakdown
-
 
 export function TimelineBreakdown ({Navigation} ) {
-    
+
     <view> 
     <image>
         dropdownicon
     </image>
+
+    <Button title = 'Expand' />
+  
 </view>
 }
 
+const styles = Stylesheet.create ({
 
-// To close it, click it again 
 
-// to mark a task as complete, tap it and turn it green 
+})
