571e34ea4711ee68f3705b933d6ba3039b2565b0
hanasafia <hanasafiaahmad@gmail.com>
2023-11-07 15:49:52 -0600

    removed unnecessary imports

--- components/timelines/timelineBreakdown.js
+++ components/timelines/timelineBreakdown.js
@@ -1,8 +1,6 @@
 import { FONTS } from "../fonts"
 import { COLORS } from "../colors"
 import { STEP_STATUS } from "./StepStatus"
-import {Image} from "../dropdownicon.png"
-import {Image} from "../inprogress.png"
 import React from "react"
 import {View, Text, Image, ScrollView, TextInput, 
     TouchableOpacity, Button, Stylesheet} from 'react-native';
